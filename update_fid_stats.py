bc6a795da52c01bdc7da5d99d82b987e8cf56b79
Tom Aldcroft <taldcroft@gmail.com>
2022-06-09 11:38:43 -0400

    Initial functioning update_fid_stats in Python

--- update_fid_stats.py
+++ update_fid_stats.py
@@ -4,6 +4,17 @@
 # #
 # ##########################################################################################
 
+from pathlib import Path
+
+import numpy as np
+import Ska.DBI
+from astropy.table import Table, vstack
+from chandra_aca.transform import count_rate_to_mag
+from cxotime import CxoTime
+from kadi.commands import get_observations
+from mica.archive import asp_l1
+from ska_helpers.utils import basic_logger
+
 # use File::Basename;
 # use Getopt::Long;
 # use PDL;
@@ -60,25 +71,30 @@
 #   create index fid_stats_obsid_index on fid_stats ( obsid ) ;
 # CREATE_TABLE_SQL
 
+__version__ = "0.1"
+
+LOGGER = basic_logger(__name__)
+LOGGER.setLevel("DEBUG")
+
 TABLE_SQL = """
 create table fid_stats (
     obsid int,
-    id_num nt,
-    id_string varchar(0),
-    tstart flat,
-    date_obs varchar(0),
-    ang_y_med flat,
-    ang_z_med flat,
-    ang_y_5th flat,
-    ang_y_95th flat,
-    ang_z_5th flat,
-    ang_z_95th flat,
-    mag_med  flat,
-    mag_i_avg flat,
-    exp_time  flat,
-    sim_z_offset flat,
-    ap_date datetme,
-    proc_status varchar40)
+    id_num int,
+    id_string varchar(10),
+    tstart float,
+    date_obs varchar(21),
+    ang_y_med float,
+    ang_z_med float,
+    ang_y_5th float,
+    ang_y_95th float,
+    ang_z_5th float,
+    ang_z_95th float,
+    mag_med  float,
+    mag_i_avg float,
+    exp_time  float,
+    sim_z_offset float,
+    ap_date datetime,
+    proc_status varchar(40)
 ) ;
 create index fid_stats_obsid_index on fid_stats ( obsid ) ;
 """
@@ -99,122 +115,190 @@ create index fid_stats_obsid_index on fid_stats ( obsid ) ;
 #             obsid => '%d',
 # 	    );
 
-FIDPROPS_COLS = ('slot', 'mag_i_avg', 'id_string', 'id_num', 'id_status')
-FORMATS = dict(
-    detector = '%s',
-	    ang_y_med='%.2f',
-	    ang_z_med='%.2f',
-	    ang_y_95th='%.2f',
-	    ang_z_95th='%.2f',
-	    ang_y_5th='%.2f',
-	    ang_z_5th='%.2f',
-	    mag_med='%.3f',
-	    mag_i_avg='%.3f',
-	    exp_time='%.2f',
-	    sim_z_offset='%.1f',
-	    tstart='%.1f',
-            obsid='%d',
-	    )
+FIDPROPS_COLS = ("slot", "mag_i_avg", "id_string", "id_num", "id_status")
+FORMAT_ROUND = dict(
+    ang_y_med=2,
+    ang_z_med=2,
+    ang_y_95th=2,
+    ang_z_95th=2,
+    ang_y_5th=2,
+    ang_z_5th=2,
+    mag_med=3,
+    mag_i_avg=3,
+    exp_time=2,
+    sim_z_offset=1,
+    tstart=1,
+)
 
-# ##****************************************************************************
-# # MAIN PROGRAM
-# ##****************************************************************************
 
-# our $fid_table = 'fid_stats';
+class NoArchiveFilesFound(FileNotFoundError):
+    pass
 
-# get_options();			# Parse command line options
-# my $dbh = sql_connect('sybase-aca-aca_ops');
 
-# # Delete the specified (comma-sep) list of obsids first.  Mostly for testing.
-# delete_obs($dbh) if $par{delete};
+def main():
+    # our $fid_table = 'fid_stats';
+    # get_options();			# Parse command line options
+    # my $dbh = sql_connect('sybase-aca-aca_ops');
+    opt = get_options()
 
-# my @obs = get_obs($dbh);
-# my @obsids = map { $_->{obsid} } @obs;
-# # Extract the list of observations
-# log_message("Processing obsids @obsids");
+    data_dir = Path(opt.data_dir)
+    db3_path = data_dir / "fid_stats.db3"
 
-# foreach $obs (@obs) {
-#     clean_up($obsid);           # Make completely sure no old files are hanging around
-#     @loglines = ();             # Clear logged messages for this obsid
-#     my $error;
-#     $obsid = $obs->{obsid};
-#     $date = localtime;
-#     log_message("********* PROCESSING OBSID $obsid at $date ***********\n");
+    if not db3_path.exists():
+        with Ska.DBI.DBI(dbi="sqlite", server=db3_path) as dbh:
+            dbh.execute(TABLE_SQL)
 
-#     my @rows = sql_fetchall_array_of_hashref($dbh,
-#                                              qq{select obsid from $fid_table
-#                                                 where obsid=$obsid});
-#     # Delete existing values if needed
-#     if (@rows) {
-#         my $cmd = qq{delete from $fid_table where obsid=$obsid};
-#         $par{readonly} ? print("$cmd\n") : sql_do($dbh, $cmd);
-#     }
+    # my @obs = get_obs($dbh);
+    # my @obsids = map { $_->{obsid} } @obs;
+    # # Extract the list of observations
+    # log_message("Processing obsids @obsids");
+    obss = get_observations(opt.start, opt.stop)
 
-#     # Do some processing for each obsid.  If any step along the way fails,
-#     # bail out to the clean-up step
+    with Ska.DBI.DBI(dbi="sqlite", server=db3_path) as dbh:
+        # # Delete the specified (comma-sep) list of obsids first.  Mostly for testing.
+        # delete_obs($dbh) if $par{delete};
+        if opt.delete:
+            delete_obs(dbh, opt.delete)
 
-#     unless (get_fidprops_acacen_files()) {
-#         $error = "Could not find fidprops or acacen files";
-# 	log_message($error);
-# 	goto CLEAN_UP;
-#     }
-#     @fidpr = ($fidpr[0]);  # Only want the first fidpr file
+        for obs in obss:
+            process_obs(dbh, obs)
 
-#     unless (merge_files()) {
-#         $error = "Could not merge acacen files";
-# 	log_message($error);
-# 	goto CLEAN_UP;
-#     }
 
-#     unless (@fidprops = read_fidprops()) {
-#         $error = "Could not read fidprops";
-# 	log_message($error);
-# 	goto CLEAN_UP;
-#     }
+def process_obs(dbh, obs):
+    # my $obsid = $obs->{obsid};
+    obsid = obs["obsid"]
+    if obsid > 38000:
+        return
 
-#     unless (@fid_stats = calc_stats()) {
-#         $error = "Could not calculate statistics files";
-# 	log_message($error);
-# 	goto CLEAN_UP;
-#     }
+    # foreach $obs (@obs) {
+    #     clean_up($obsid);           # Make completely sure no old files are hanging around
+    #     @loglines = ();             # Clear logged messages for this obsid
+    #     my $error;
+    #     $obsid = $obs->{obsid};
+    #     $date = localtime;
+    #     log_message("********* PROCESSING OBSID $obsid at $date ***********\n");
 
-#     log_message("Adding fid stats to database");
-#     if (not $par{readonly}) {
-#         for my $fid_stat (@fid_stats) {
-#             $fid_stat->{ap_date} = $obs->{last_ap_date};
+    #     my @rows = sql_fetchall_array_of_hashref($dbh,
+    #                                              qq{select obsid from $fid_table
+    #                                                 where obsid=$obsid});
+    #     # Delete existing values if needed
+    #     if (@rows) {
+    #         my $cmd = qq{delete from $fid_table where obsid=$obsid};
+    #         $par{readonly} ? print("$cmd\n") : sql_do($dbh, $cmd);
+    #     }
 
-#             eval { sql_insert_hash($dbh, $fid_table, $fid_stat) };
-#             if ($@) {
-#                 log_message($@);
-#                 $error = substr($@, 0, 39);
-#                 goto CLEAN_UP;
-#             }
-#         }
-#     }
+    # Check if this is in the database already. Unlike the perl version this
+    # does not typically update existing entries.
+    fids = dbh.fetchall(f"select * from fid_stats where obsid={obsid}")
+    if len(fids) > 0:
+        LOGGER.debug(f"Obsid {obsid} already in database, skipping")
+        return
 
-#   CLEAN_UP:
-#     # In case of any error, remove database rows (which may be corrupted)
-#     # and put in an entry with the error message.
-#     if (defined $error) {
-#         my $cmd = qq{delete from $fid_table where obsid=$obsid};
-#         if ($par{readonly}) {
-#             print "$cmd\n";
-#             print "Insert process status error message\n";
-#         } else {
-#             sql_do($dbh, $cmd);
-#             sql_insert_hash($dbh, $fid_table, {obsid => $obsid,
-#                                                proc_status => $error} );
-#         }
-#     }
+    LOGGER.info(f"***** PROCESSING OBSID {obsid} at {obs['obs_start']} *****")
 
-#     clean_up($obsid, $error);
-# }
+    #     # Do some processing for each obsid.  If any step along the way fails,
+    #     # bail out to the clean-up step
+    fidprops = get_archive_file_data(obs, "FIDPROPS", max_files=1)
+    if fidprops is None:
+        return
 
-# # Close down arc5gl properly (somehow normal garbage collection doesn't work here)
-# if (defined $Ska::Process::arc5gl) {
-#     undef $Ska::Process::arc5gl;
-#     sleep 2;
-# }
+    acen = get_archive_file_data(obs, "ACACENT")
+    if acen is None:
+        return
+
+    LOGGER.info(f"Got {len(fidprops)} rows fidprops and {len(acen)} rows acacen")
+
+    stats = calc_fid_stats(obs, fidprops, acen)
+
+    for stat in stats:
+        LOGGER.debug(f"Inserting row {stat}")
+        dbh.insert(stat, "fid_stats")
+
+
+def get_archive_file_data(obs, content, max_files=None):
+    files = asp_l1.get_files(
+        start=obs["obs_start"], stop=obs["obs_stop"], content=[content]
+    )
+
+    if len(files) == 0:
+        LOGGER.info(
+            f"No {content} files found for obsid {obs['obsid']} between "
+            f"{obs['obs_start']} and {obs['obs_stop']}"
+        )
+        return None
+
+    if max_files is not None and len(files) > max_files:
+        LOGGER.error(
+            f"Too many {content} files found for obsid {obs['obsid']} between "
+            f"{obs['obs_start']} and {obs['obs_stop']}"
+        )
+        return None
+
+    dats = [Table.read(file) for file in files]
+    LOGGER.debug(f"Got {len(dats)} {content} files")
+    if len(dats) == 1:
+        out = dats[0]
+    else:
+        out = vstack(dats, join_type="exact", metadata_conflicts="silent")
+
+    return out
+
+    #     unless (merge_files()) {
+    #         $error = "Could not merge acacen files";
+    # 	log_message($error);
+    # 	goto CLEAN_UP;
+    #     }
+
+    #     unless (@fidprops = read_fidprops()) {
+    #         $error = "Could not read fidprops";
+    # 	log_message($error);
+    # 	goto CLEAN_UP;
+    #     }
+
+    #     unless (@fid_stats = calc_stats()) {
+    #         $error = "Could not calculate statistics files";
+    # 	log_message($error);
+    # 	goto CLEAN_UP;
+    #     }
+
+    #     log_message("Adding fid stats to database");
+    #     if (not $par{readonly}) {
+    #         for my $fid_stat (@fid_stats) {
+    #             $fid_stat->{ap_date} = $obs->{last_ap_date};
+
+    #             eval { sql_insert_hash($dbh, $fid_table, $fid_stat) };
+    #             if ($@) {
+    #                 log_message($@);
+    #                 $error = substr($@, 0, 39);
+    #                 goto CLEAN_UP;
+    #             }
+    #         }
+    #     }
+
+    #   CLEAN_UP:
+    #     # In case of any error, remove database rows (which may be corrupted)
+    #     # and put in an entry with the error message.
+    #     if (defined $error) {
+    #         my $cmd = qq{delete from $fid_table where obsid=$obsid};
+    #         if ($par{readonly}) {
+    #             print "$cmd\n";
+    #             print "Insert process status error message\n";
+    #         } else {
+    #             sql_do($dbh, $cmd);
+    #             sql_insert_hash($dbh, $fid_table, {obsid => $obsid,
+    #                                                proc_status => $error} );
+    #         }
+    #     }
+
+    #     clean_up($obsid, $error);
+    # }
+
+    # # Close down arc5gl properly (somehow normal garbage collection doesn't work here)
+    # if (defined $Ska::Process::arc5gl) {
+    #     undef $Ska::Process::arc5gl;
+    #     sleep 2;
+    # }
+
 
 # ##****************************************************************************
 # sub get_options {
@@ -241,6 +325,31 @@ FORMATS = dict(
 
 # }
 
+
+def get_options(sys_args=None):
+    import argparse
+
+    # RESTORE THIS LINE
+    # from . import __version__
+
+    parser = argparse.ArgumentParser(description=f"get_fid_data {__version__}")
+    parser.add_argument("--start", type=str, help="Start date")
+    parser.add_argument("--stop", type=str, help="Stop date")
+    parser.add_argument(
+        "--data-dir", type=str, default=".", help="Data directory (default='.')"
+    )
+    parser.add_argument(
+        "--delete", type=str, help="Comma-separated list of obsids to delete"
+    )
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        help="logging level (debug, info, warning, error, critical)",
+    )
+    args = parser.parse_args(sys_args)
+    return args
+
+
 # ##****************************************************************************
 # sub delete_obs {
 # ##****************************************************************************
@@ -254,41 +363,16 @@ FORMATS = dict(
 #     }
 # }
 
+
+def delete_obs(dbh, delete_obsids):
+    pass
+
+
 # ##****************************************************************************
 # sub get_obs {
 # ##****************************************************************************
 #     my $dbh = shift;
 
-#     my $newer_AP_query = <<NEWER_AP_QUERY ;
-# SELECT distinct mp.obsid, mp.last_ap_date
-#   FROM fid_stats f, mp_load_info mp
-#   WHERE f.obsid = mp.obsid
-#      AND f.ap_date < mp.last_ap_date
-#      AND f.proc_status IS NULL
-# NEWER_AP_QUERY
-# ;
-#     # log_message("Fetching list of obsids with updated processing");
-#     # my @process_obs = sql_fetchall_array_of_hashref($dbh, $newer_AP_query);
-#     # For some reason not understood the newer_AP_query returns everything.
-#     # Dates that are apparently the same in sqsh are satisfying the
-#     # f.ap_date < mp.last_ap_date clause.  So for now don't check for
-#     # updated processing:
-#     my @process_obs = ();
-
-#     log_message("Fetching existing fid_stats obsids");
-#     my @fid_obsids = sql_fetchall_array_of_hashref($dbh,
-#                                                   qq{select distinct obsid from fid_stats});
-#     my %fid_obsids = map { $_->{obsid} => 1 } @fid_obsids;
-#     log_message("Fetching mp_load_info obsids");
-#     my @mp_obs = sql_fetchall_array_of_hashref($dbh,
-#                                            qq{SELECT obsid, last_ap_date FROM mp_load_info
-#                                               WHERE obsid < 40000 });
-#     for my $mp_obs (@mp_obs) {
-#         push @process_obs, $mp_obs unless $fid_obsids{$mp_obs->{obsid}};
-#     }
-
-#     return reverse @process_obs;
-# }
 
 # ##****************************************************************************
 # sub get_fidprops_acacen_files {
@@ -451,12 +535,85 @@ FORMATS = dict(
 #     return @fid_stats;
 # }
 
-# ##***************************************************************************
-# sub log_message {
-# ##***************************************************************************
-#     my ($msg) = @_;
 
-#     print "$msg\n" if ($par{loud});
-#     push @loglines, "$msg\n";
-# }
+def calc_fid_stats(obs, fidprops, acen):
+    """
+    Calculate fid statistics
+    """
+    stats = []
+    for fidpr in fidprops:
+        if fidpr["id_status"].strip() != "GOOD":
+            LOGGER.info(
+                f"WARNING: obsid {obs['obsid']} has bad fid status {fidpr['id_status']!r} in slot {fidpr['slot']}"
+            )
+            continue
+        stat = calc_stats_for_fidpr(obs, acen, fidpr)
+        if stat is not None:
+            stats.append(stat)
 
+    return stats
+
+
+def calc_stats_for_fidpr(obs, acen, fidpr):
+    t_int = 1.696
+    slot = fidpr["slot"]
+    ok = (acen["slot"] == slot) & (acen["alg"] == 8) & (acen["status"] == 0)
+    cen = acen[ok]
+    n_cen = len(cen)
+
+    if n_cen < 200:
+        LOGGER.info(
+            f"WARNING: obsid {obs['obsid']} has {n_cen} < 200 "
+            f"fid counts in slot {slot}, skipping"
+        )
+        return
+
+    ang_y_sm = cen["ang_y_sm"] * 3600
+    ang_z_sm = cen["ang_z_sm"] * 3600
+    # SIM_Z offset in arcsec (using 20.493 arcsec/mm)
+    sim_z_offset = (fidpr.meta["LSI0STT3"] + fidpr.meta["STT0STF3"]) * 20.493
+    stat = {
+        "id_num": fidpr["id_num"],
+        "id_string": fidpr["id_string"],
+        "tstart": CxoTime(obs["obs_start"]).secs,
+        "obsid": obs["obsid"],
+        "date_obs": obs["obs_start"],
+        # CHANGE ME TO :1000 (use first 4 ksec not last 4 ksec)
+        "ang_y_med": np.median(ang_y_sm[-1000:]),
+        "ang_z_med": np.median(ang_z_sm[-1000:]),
+        "ang_y_5th": np.percentile(ang_y_sm, 5),
+        "ang_y_95th": np.percentile(ang_y_sm, 95),
+        "ang_z_5th": np.percentile(ang_z_sm, 5),
+        "ang_z_95th": np.percentile(ang_z_sm, 95),
+        "mag_med": count_rate_to_mag(np.median(cen["counts"]) / t_int),
+        "mag_i_avg": fidpr["mag_i_avg"],
+        "exp_time": acen.meta["TSTOP"] - acen.meta["TSTART"],
+        "sim_z_offset": sim_z_offset,
+    }
+
+    # Make the numbers prettier
+    for key, precision in FORMAT_ROUND.items():
+        stat[key] = round(stat[key], precision)
+
+    return stat
+
+
+#            $data = { id_num => $fidpr->{id_num},
+#                       id_string => $fidpr->{id_string},
+#                       tstart => $cen->time->at(0),
+#                       obsid => $acen_hdr->{OBS_ID},
+#                       date_obs => $acen_hdr->{'DATE-OBS'},
+#                       ang_y_med => median($cen->ang_y_sm->($ok0:)) * 3600,
+#                       ang_z_med => median($cen->ang_z_sm->($ok0:)) * 3600,
+#                       ang_y_5th => $ang_y->at(int($ncen*0.05)),
+#                       ang_y_95th => $ang_y->at(int($ncen*0.95)),
+#                       ang_z_5th => $ang_z->at(int($ncen*0.05)),
+#                       ang_z_95th => $ang_z->at(int($ncen*0.95)),
+#                       mag_med  => $mag_med,
+#                       mag_i_avg => $fidpr->{mag_i_avg},
+#                       exp_time => ($acen_hdr->{TSTOP} - $acen_hdr->{TSTART}) / 1000.,
+#                       sim_z_offset => ($fidpr_hdr->{LSI0STT3} + $fidpr_hdr->{STT0STF3}) * 20.493, # arcsec/mm
+
+
+if __name__ == "__main__":
+    main()
