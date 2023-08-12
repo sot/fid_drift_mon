a97acfb4a2860c842c42ffaff31101794f60693e
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-08-12 09:47:57 -0400

    Python 3 and update utils/calc_abs_cel_pointing

--- utils/calc_abs_cel_pointing.py
+++ utils/calc_abs_cel_pointing.py
@@ -4,7 +4,6 @@ Compute the 99th percentile radius for fid light offsets over the last year.
   % ipython --pylab
 
   >>> run calc_abs_cel_pointing.py
-  >>> calc_all_dets()
   Fid ACIS-S 3
   Not enough readouts for the median
   99th percentile radius for ACIS-S fid light offset from median is 12.1
@@ -16,28 +15,38 @@ Compute the 99th percentile radius for fid light offsets over the last year.
 
   99th percentile radius for fid light offset from median is 11.8 arcsec
 """
-
+import argparse
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import astropy.units as u
 
-import Ska.DBI
-from Chandra.Time import DateTime
+import ska_dbi
+from cxotime import CxoTime
+
+from fid_drift_mon.paths import FID_STATS_PATH
 
 
-def get_fid_stats(det, start=None):
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="Plot drift model")
+    parser.add_argument(
+        "--data-dir", type=str, default=".", help="Output data directory"
+    )
+    args = parser.parse_args(args)
+    return args
+
+
+def get_fid_stats(db, det, start=None):
     if start is None:
-        start = DateTime() - 365.0
-    tstart = DateTime(start).secs
-    query = ('select id_num, id_string, tstart, ang_y_med, ang_z_med, sim_z_offset'
-             ' FROM fid_stats'
-             ' WHERE proc_status IS NULL'
-             '   AND id_string LIKE "{}%"'
-             '   AND tstart > {}'
-             .format(det, tstart))
-
-    db = Ska.DBI.DBI(dbi='sybase', server='sybase', user='aca_read')
+        start = CxoTime.now() - 365.0 * u.day
+    tstart = CxoTime(start).secs
+    query = (
+        "select id_num, id_string, tstart, ang_y_med, ang_z_med, sim_z_offset"
+        " FROM fid_stats"
+        " WHERE proc_status IS NULL"
+        '   AND id_string LIKE "{}%"'
+        "   AND tstart > {}".format(det, tstart)
+    )
     vals = db.fetchall(query)
     db.conn.close()
 
@@ -45,61 +54,81 @@ def get_fid_stats(det, start=None):
 
 
 def calc_abs_cel_pointing(detstats, det):
-    year0 = 0.
-    fids = {'ACIS-S': [1, 2, 3, 4, 5, 6],
-            'ACIS-I': [1, 2, 3, 4, 5, 6],
-            'HRC-I': [7, 8, 9, 10],
-            'HRC-S': [11, 12, 13, 14],
-            }
-    fidcolor = ' bgrcmkbgrcbgrc'
-    sim_z_nom = np.median(detstats['sim_z_offset'])  # median for this detector
+    year0 = 0.0
+    fids = {
+        "ACIS-S": [1, 2, 3, 4, 5, 6],
+        "ACIS-I": [1, 2, 3, 4, 5, 6],
+        "HRC-I": [7, 8, 9, 10],
+        "HRC-S": [11, 12, 13, 14],
+    }
+    fidcolor = " bgrcmkbgrcbgrc"
+    sim_z_nom = np.median(detstats["sim_z_offset"])  # median for this detector
     plt.figure(figsize=(6, 4.5))
     plt.clf()
     plt.subplot(2, 1, 1)
-    plt.ylabel('Y offset (arcsec)')
-    plt.title(det + ' Fid Drift')
+    plt.ylabel("Y offset (arcsec)")
+    plt.title(det + " Fid Drift")
     plt.grid()
     plt.subplot(2, 1, 2)
-    plt.xlabel('Year')  # MET (years)')
-    plt.ylabel('Z offset (arcsec)')
+    plt.xlabel("Year")  # MET (years)')
+    plt.ylabel("Z offset (arcsec)")
     plt.grid()
     drs = []
     for fid in fids[det]:
-        fidstats = detstats[detstats['id_num'] == fid]
-        year = fidstats['tstart'] / 86400. / 365.25 + 1998.0
+        fidstats = detstats[detstats["id_num"] == fid]
+        year = fidstats["tstart"] / 86400.0 / 365.25 + 1998.0
         fidstatsnorm = fidstats
         if len(fidstatsnorm) < 3:
-            print 'Fid %s %d' % (det, fid)
-            print 'Not enough readouts for the median'
+            print("Fid %s %d" % (det, fid))
+            print("Not enough readouts for the median")
             continue
-        y0 = np.median(fidstatsnorm['ang_y_med'])
-        z0 = np.median(fidstatsnorm['ang_z_med'] + fidstatsnorm.sim_z_offset - sim_z_nom)
+        y0 = np.median(fidstatsnorm["ang_y_med"])
+        z0 = np.median(
+            fidstatsnorm["ang_z_med"] + fidstatsnorm.sim_z_offset - sim_z_nom
+        )
         plt.subplot(2, 1, 1)
-        dy = fidstats['ang_y_med'] - y0
-        dz = fidstats['ang_z_med'] - z0 + fidstats['sim_z_offset'] - sim_z_nom
-        dr = np.sqrt(dy ** 2 + dz ** 2)
-        plt.plot(year - year0, dy, ',', markerfacecolor=fidcolor[fid], mew=0)
+        dy = fidstats["ang_y_med"] - y0
+        dz = fidstats["ang_z_med"] - z0 + fidstats["sim_z_offset"] - sim_z_nom
+        dr = np.sqrt(dy**2 + dz**2)
+        plt.plot(year - year0, dy, ",", markerfacecolor=fidcolor[fid], mew=0)
         plt.subplot(2, 1, 2)
-        plt.plot(year - year0, dz, ',', markerfacecolor=fidcolor[fid], mew=0)
+        plt.plot(year - year0, dz, ",", markerfacecolor=fidcolor[fid], mew=0)
         drs.append(dr)
 
     dr = np.concatenate(drs)
     dr99 = np.percentile(dr, 99.0)
-    print '99th percentile radius for {} fid light offset from median is {:.1f}'.format(det, dr99)
+    print(
+        "99th percentile radius for {} fid light offset from median is {:.1f}".format(
+            det, dr99
+        )
+    )
 
     return dr
 
 
-def calc_all_dets():
-    dets = ('ACIS-S', 'ACIS-I', 'HRC-S', 'HRC-I')
+def main(args=None):
+    args = parse_args(args)
+
+    matplotlib.use("Agg")
+
+    dets = ("ACIS-S", "ACIS-I", "HRC-S", "HRC-I")
 
     drs = []
     for det in dets:
-        detstats = get_fid_stats(det)
+        with ska_dbi.DBI(dbi="sqlite", server=FID_STATS_PATH(args.data_dir)) as dbh:
+            detstats = get_fid_stats(dbh, det)
         dr = calc_abs_cel_pointing(detstats, det)
         drs.append(dr)
 
     dr = np.concatenate(drs)
     dr99 = np.percentile(dr, 99.0)
-    print
-    print '99th percentile radius for fid light offset from median is {:.1f} arcsec'.format(dr99)
+    print()
+    print(
+        "99th percentile radius for fid light offset from median is {:.1f} arcsec".format(
+            dr99
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
